80f41035214660dd9928b62fa172aa347f82204f
Gyanendra Kumar <gyan.mu@gmail.com>
2025-09-28 18:32:07 +0530

    removed conflicts

--- src/modules/auth/sign.in.tsx
+++ src/modules/auth/sign.in.tsx
@@ -179,14 +179,14 @@ const SignInView = () => {
                   Sign In
                 </Button>
 
-                {/* <div className="after:border-border relative text-center tex-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <div className="after:border-border relative text-center tex-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                   <span className="bg-card text-muted-foreground relative z-10 px-2">
                     Or continue with
                   </span>
-                </div> */}
+                </div>
 
                 {/* GOOGLE AND GITHUB TO SIGN-IN */}
-                {/* <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-2 gap-4">
                   <Button
                     variant="outline"
                     type="button"
@@ -211,7 +211,7 @@ const SignInView = () => {
                     ) : null}
                     GitHub
                   </Button>
-                </div> */}
+                </div>
 
                 <div className="text-center text-sm">
                   Don&apos;t have an account?{" "}
