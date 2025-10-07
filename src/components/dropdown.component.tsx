6256ff8026d164fe7e87d3e4c7871170b0e9f1d3
Gyanendra Kumar <gyan.mu@gmail.com>
2025-10-08 01:41:22 +0530

    working on dropdown menus

---
+++ src/components/dropdown.component.tsx
@@ -0,0 +1,45 @@
+import { cn } from "../lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+const Dropdown = ({
+  children,
+  trigger,
+  triggerClassName,
+  alignOverlay = "end",
+  overflowSide = "right",
+  onOpenChange,
+}: {
+  children: React.ReactNode;
+  trigger: React.ReactElement | null;
+  triggerClassName?: string;
+  alignOverlay?: "end" | "center" | "start";
+  overflowSide?: "right" | "left" | "top" | "bottom";
+  onOpenChange?: (open: boolean) => void;
+}) => {
+  return (
+    <DropdownMenu onOpenChange={onOpenChange}>
+      <DropdownMenuTrigger
+        className={cn(
+          "rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/10 cursor-pointer",
+          triggerClassName
+        )}
+        asChild
+      >
+        {trigger}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align={alignOverlay}
+        side={overflowSide}
+        className="w-72"
+      >
+        {children}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default Dropdown;
--- src/modules/dashboard/ui/components/dashboard.user.button.tsx
+++ src/modules/dashboard/ui/components/dashboard.user.button.tsx
@@ -1,13 +1,63 @@
+import { ChevronRight } from "lucide-react";
+import { useState } from "react";
+import Dropdown from "../../../../components/dropdown.component";
+import GenerateAvatar from "../../../../components/generate-avatar";
+import { Avatar, AvatarImage } from "../../../../components/ui/avatar";
 import { authClient } from "../../../../lib/auth-client";
 
 const DashboardUserButton = () => {
   const { data, isPending } = authClient.useSession();
+  const [open, setOpen] = useState<boolean>(false);
 
   if (isPending || !data?.user) {
     return null;
   }
 
-  return <div>user button</div>;
+  const { name, image, email } = data.user;
+
+  const dropdownTrigger = () => (
+    <button
+      className="flex items-center justify-between w-full cursor-pointer"
+      onClick={() => setOpen((prev) => !prev)}
+    >
+      {image ? (
+        <Avatar>
+          <AvatarImage src={image} />
+        </Avatar>
+      ) : (
+        <GenerateAvatar
+          seed={image ?? name}
+          variant="initials"
+          className="size-7 mr-3"
+        />
+      )}
+
+      <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+        <p className="text-sm truncate w-full">{name}</p>
+        <p className="text-xs truncate w-full">{email}</p>
+      </div>
+
+      {/* Single Chevron icon with rotation animation */}
+      <ChevronRight
+        className={`transition-transform duration-300 origin-center ${
+          !open ? "rotate-90" : "rotate-0"
+        }`}
+      />
+    </button>
+  );
+
+  return (
+    <div>
+      <Dropdown trigger={dropdownTrigger()} onOpenChange={setOpen}>
+        <div className="flex flex-col gap-1">
+          <span className="font-medium truncate">{name}</span>
+          <span className="text-sm font-normal text-muted-foreground truncate">
+            {email}
+          </span>
+        </div>
+      </Dropdown>
+    </div>
+  );
 };
 
 export default DashboardUserButton;
