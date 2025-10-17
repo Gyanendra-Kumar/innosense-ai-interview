97573e389949fefbd45948d7128b56f295dd3380
Gyanendra Kumar <gyan.mu@gmail.com>
2025-10-17 15:57:24 +0530

    added try catch for me

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,6 +1,13 @@
 import { getUser } from "../../../lib/getUser";
 
 export async function GET() {
-  const user = await getUser();
-  return Response.json(user);
+  try {
+    const user = await getUser();
+    if (!user) return Response.json({ error: "Unauthorized" }, { status: 401 });
+
+    return Response.json(user);
+  } catch (error) {
+    console.error("Error while fetching user: ", error);
+    return Response.json({ error: "Failed to fetch user: " }, { status: 500 });
+  }
 }
