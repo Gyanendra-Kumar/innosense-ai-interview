38c7331e6e33768254b6ca2f80a920c1ce5ef1c1
Gyanendra Kumar <gyan.mu@gmail.com>
2025-11-02 18:31:23 +0530

    agent setup

--- src/app/[slug]/(sidebar-items)/agents/page.tsx
+++ src/app/[slug]/(sidebar-items)/agents/page.tsx
@@ -1,7 +1,42 @@
+import ErrorState from "@/components/error-state";
+import LoadingState from "@/components/loading-state";
 import AgentView from "@/modules/agents/ui/agent-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-const Agents = () => {
-  return <AgentView />;
+const Page = async () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense
+        fallback={
+          <div className="flex items-center h-[calc(100vh-10rem)]">
+            <LoadingState
+              title="Loading Agent"
+              description="This may take a few seconds..."
+            />
+          </div>
+        }
+      >
+        <ErrorBoundary
+          fallback={
+            <div className="flex items-center h-[calc(100vh-10rem)]">
+              <ErrorState
+                title="Failed to load agent"
+                description="Something went wrong! Please try again."
+              />
+            </div>
+          }
+        >
+          <AgentView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
-export default Agents;
+export default Page;
--- src/modules/agents/ui/agent-view.tsx
+++ src/modules/agents/ui/agent-view.tsx
@@ -1,28 +1,13 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
+import ErrorState from "../../../components/error-state";
 import LoadingState from "../../../components/loading-state";
 import { useTRPC } from "../../../trpc/client";
 
 const AgentView = () => {
   const trpc = useTRPC();
-  const { data, isLoading, isError } = useQuery(
-    trpc.agents.getMany.queryOptions()
-  );
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center h-[calc(100vh-10rem)]">
-        <LoadingState
-          title="Loading Agent"
-          description="This may take a few seconds..."
-        />
-      </div>
-    );
-  }
-  if (isError) {
-    return <div>Error loading agents.</div>;
-  }
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return <div>Agent View</div>;
 };
