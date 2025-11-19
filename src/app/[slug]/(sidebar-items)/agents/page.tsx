19bf63be60868316459dfb039d5896f8c566a4c5
Gyanendra Kumar <gyan.mu@gmail.com>
2025-11-20 00:23:32 +0530

    11: agent forms

--- src/app/[slug]/(sidebar-items)/agents/page.tsx
+++ src/app/[slug]/(sidebar-items)/agents/page.tsx
@@ -5,37 +5,41 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import ListHeader from "../../../../modules/agents/ui/agent-list-header";
 
 const Page = async () => {
   const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+  await queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense
-        fallback={
-          <div className="flex items-center h-[calc(100vh-10rem)]">
-            <LoadingState
-              title="Loading Agent"
-              description="This may take a few seconds..."
-            />
-          </div>
-        }
-      >
-        <ErrorBoundary
+    <>
+      <ListHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense
           fallback={
             <div className="flex items-center h-[calc(100vh-10rem)]">
-              <ErrorState
-                title="Failed to load agent"
-                description="Something went wrong! Please try again."
+              <LoadingState
+                title="Loading Agent"
+                description="This may take a few seconds..."
               />
             </div>
           }
         >
-          <AgentView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+          <ErrorBoundary
+            fallback={
+              <div className="flex items-center h-[calc(100vh-10rem)]">
+                <ErrorState
+                  title="Failed to load agent"
+                  description="Something went wrong! Please try again."
+                />
+              </div>
+            }
+          >
+            <AgentView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   );
 };
 
--- src/modules/agents/ui/agent-view.tsx
+++ src/modules/agents/ui/agent-view.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
-import ErrorState from "../../../components/error-state";
-import LoadingState from "../../../components/loading-state";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "../../../trpc/client";
-import { ResponsiveDialog } from "@/components/responsive-dialog";
-import { Button } from "@/components/ui/button";
 
 const AgentView = () => {
   const trpc = useTRPC();
@@ -13,13 +9,7 @@ const AgentView = () => {
 
   return (
     <div>
-      {/*<ResponsiveDialog
-        title="Responsive test"
-        description="Responsive description" open onOpenChange={() => {}}
-      >
-        <Button>Some Actions</Button>
-      </ResponsiveDialog>*/}
-      <p>Agent View</p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 };
